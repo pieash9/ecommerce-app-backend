3d9f0a7f39316e0193c3723f4322218b4f8ff0d5
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-06-14 12:49:33 +0600

    added error handler middleware

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,58 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { prismaClient } from "..";
+import { compareSync, hashSync } from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
+import { BadRequestException } from "../exceptions/bad-request";
+import { ErrorCode } from "../exceptions/root";
 
-export const login = (req: Request, res: Response) => {
-  res.send("Login works");
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({
+    where: { email },
+  });
+
+  if (user) {
+    next(
+      new BadRequestException(
+        "User already exists!",
+        ErrorCode.USER_ALREADY_EXISTS
+      )
+    );
+  }
+
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+    },
+  });
+
+  res.json(user);
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({
+    where: { email },
+  });
+
+  if (!user) {
+    throw Error("User does not exist!");
+  }
+
+  if (!compareSync(password, user.password)) {
+    throw Error("Incorrect password!");
+  }
+
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET as string);
+
+  res.json({ user, token });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
+import { errorMiddleware } from "./middlewares/errors";
+import { SignupSchema } from "./schema/users";
 
 const app: Express = express();
 
+app.use(express.json());
+
 app.use("/api", rootRouter);
 
-app.listen(PORT, () => console.log("App Working"));
+export const prismaClient = new PrismaClient().$extends({
+  query: {
+    user: {
+      create({ args, query }) {
+        args.data = SignupSchema.parse(args.data);
+        return query(args);
+      },
+    },
+  },
+});
+
+app.use(errorMiddleware);
+
+app.listen(PORT, () => console.log("App is running on", PORT));
