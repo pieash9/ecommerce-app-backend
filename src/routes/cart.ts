17b2b466a1130713c562a1944bb37ca899ccff19
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-06-15 21:33:51 +0600

    added add to cart routes

---
+++ src/routes/cart.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { authMiddleware } from "../middlewares/auth";
+import { errorHandler } from "../error-handler";
+import {
+  addItemToCart,
+  changeQuantity,
+  deleteItemFromCart,
+  getCart,
+} from "../controllers/cart";
+
+const cartRoutes = Router();
+
+cartRoutes.post("/", [authMiddleware], errorHandler(addItemToCart));
+cartRoutes.get("/", [authMiddleware], errorHandler(getCart));
+cartRoutes.delete("/", [authMiddleware], errorHandler(deleteItemFromCart));
+cartRoutes.put("/:id", [authMiddleware], errorHandler(changeQuantity));
+
+export default cartRoutes;
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,6 +3,6 @@ import { User } from "@prisma/client";
 
 declare module "express" {
   export interface Request {
-    user?: User;
+    user: User;
   }
 }
