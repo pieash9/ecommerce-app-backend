2378225a5534a8db735135ece5c6d16271820f89
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-06-16 12:25:34 +0600

    complete order route

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
 
 export const createOrder = async (req: Request, res: Response) => {
   // 1.to create a transaction
@@ -66,8 +68,55 @@ export const createOrder = async (req: Request, res: Response) => {
   });
 };
 
-export const listOrders = async (req: Request, res: Response) => {};
+export const listOrders = async (req: Request, res: Response) => {
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: req.user.id,
+    },
+  });
 
-export const cancelOrder = async (req: Request, res: Response) => {};
+  res.json(orders);
+};
 
-export const getOrderById = async (req: Request, res: Response) => {};
+export const cancelOrder = async (req: Request, res: Response) => {
+  try {
+    const order = await prismaClient.order.update({
+      where: {
+        userId: req.user.id,
+        id: +req.params.id,
+      },
+      data: {
+        status: "CANCELLED",
+      },
+    });
+
+    await prismaClient.orderEvents.create({
+      data: {
+        orderId: order.id,
+        status: "CANCELLED",
+      },
+    });
+
+    res.json(order);
+  } catch (error) {
+    throw new NotFoundException("Order not found", ErrorCode.ORDER_NOT_FOUND);
+  }
+};
+
+export const getOrderById = async (req: Request, res: Response) => {
+  try {
+    const order = await prismaClient.order.findFirstOrThrow({
+      where: {
+        id: +req.params.id,
+      },
+      include: {
+        products: true,
+        events: true,
+      },
+    });
+
+    res.json(order);
+  } catch (error) {
+    throw new NotFoundException("Order not found", ErrorCode.ORDER_NOT_FOUND);
+  }
+};
